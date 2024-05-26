7cc7cf66af5f854da6d4225a96561a39fa5b930c
Giant77 <willyarsyad77@gmail.com>
2024-05-26 11:22:59 +0700

    add routes, and auth pages

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::view('/login', 'auth.login')->name('login');
+Route::post('/postLogin', [AuthController::class, 'postLogin'])->name('postlogin');
